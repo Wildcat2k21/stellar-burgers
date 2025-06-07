8ecc5e8a2ba63c70c960432d3e6774bd0e31b81d
wildcat2k21 <djerse12@mail.ru>
2025-06-07 16:57:58 +0300

    после каждого клика на «Добавить» вызываем expectIngredientInConstructor, чтобы проверить наличие нужного ингридиента, убеждаемся, что [data-cy=burger-constructor] действительно содержит этот текст

--- cypress/e2e/constructor-page.cy.ts
+++ cypress/e2e/constructor-page.cy.ts
@@ -3,7 +3,7 @@ const appURL = 'http://localhost:4000';
 const constructorElementBun1 = `[data-cy=constructor-element-bun1]`;
 const constructorElementMain = `[data-cy=constructor-element-main]`;
 const constructorElementBun2 = `[data-cy=constructor-element-bun2]`;
-const burgerIngredient = (_id: string) => `[data-cy=burger-ingredient-${_id}]`;
+const burgerIngredient = (_id) => `[data-cy=burger-ingredient-${_id}]`;
 const modal = `[data-cy=modal]`;
 const closeModalButton = `[data-cy=close-modal-button]`;
 const modalOverlay = `[data-cy=modal-overlay]`;
@@ -25,55 +25,78 @@ afterEach(() => {
   cy.clearAllLocalStorage();
 });
 
+/**
+ * Утилита-обёртка:
+ *  по ID берём полное имя ингредиента из фикстуры
+ *  и ассертом проверяем, что оно присутствует в конструкторе.
+ */
+function expectIngredientInConstructor(_id) {
+  cy.fixture('ingredients.json').then(({ data }) => {
+    const { name } = data.find((i) => i._id === _id);
+    cy.get(burgerConstructor).contains(name).should('exist');
+  });
+}
+
 describe('Интеграционное тестирование cypress', () => {
   it('Добавление ингредиентов в конструктор', () => {
-    const _id1 = '643d69a5c3f7b9001cfa093c'; // булка
+    const bunId = '643d69a5c3f7b9001cfa093c';   // булка
+    const meatId = '643d69a5c3f7b9001cfa0941';  // котлета
+
+    // БУЛКА
     cy.get(constructorElementBun1).should('exist');
     cy.get(constructorElementBun2).should('exist');
-    cy.get(burgerIngredient(_id1)).contains('Добавить').click();
+    cy.get(burgerIngredient(bunId)).contains('Добавить').click();
     cy.get(constructorElementBun1).should('not.exist');
     cy.get(constructorElementBun2).should('not.exist');
+    expectIngredientInConstructor(bunId);       // <-- точная проверка
 
-    const _id2 = '643d69a5c3f7b9001cfa0941'; // котлета
+    // НАЧИНКА
     cy.get(constructorElementMain).should('exist');
-    cy.get(burgerIngredient(_id2)).contains('Добавить').click();
+    cy.get(burgerIngredient(meatId)).contains('Добавить').click();
     cy.get(constructorElementMain).should('not.exist');
+    expectIngredientInConstructor(meatId);      // <-- точная проверка
   });
 
   it('Модальное окно ингредиента', () => {
-    const _id1 = '643d69a5c3f7b9001cfa093c';
+    const bunId = '643d69a5c3f7b9001cfa093c';
+    const meatId = '643d69a5c3f7b9001cfa0941';
+
     cy.get(modal).should('not.exist');
-    cy.get(burgerIngredient(_id1)).click(); // кнопка
+    cy.get(burgerIngredient(bunId)).click();    // кнопка
     cy.get(modal).should('exist');
     cy.get(closeModalButton).click();
     cy.get(modal).should('not.exist');
 
-    const _id2 = '643d69a5c3f7b9001cfa0941';
     cy.get(modal).should('not.exist');
-    cy.get(burgerIngredient(_id2)).click();
+    cy.get(burgerIngredient(meatId)).click();
     cy.get(modal).should('exist');
     cy.get(modalOverlay).click({ force: true }); // оверлей
     cy.get(modal).should('not.exist');
   });
 
   it('Оформление заказа', () => {
-    const _id1 = '643d69a5c3f7b9001cfa093c'; // булка
+    const bunId = '643d69a5c3f7b9001cfa093c';   // булка
+    const meatId = '643d69a5c3f7b9001cfa0941';  // котлета
+    const orderNumber = '11111';                // поле number заказа
+
+    // Добавляем булку
     cy.get(constructorElementBun1).should('exist');
     cy.get(constructorElementBun2).should('exist');
-    cy.get(burgerIngredient(_id1)).contains('Добавить').click();
+    cy.get(burgerIngredient(bunId)).contains('Добавить').click();
     cy.get(constructorElementBun1).should('not.exist');
     cy.get(constructorElementBun2).should('not.exist');
+    expectIngredientInConstructor(bunId);
 
-    const _id2 = '643d69a5c3f7b9001cfa0941'; // котлета
+    // Добавляем начинку
     cy.get(constructorElementMain).should('exist');
-    cy.get(burgerIngredient(_id2)).contains('Добавить').click();
+    cy.get(burgerIngredient(meatId)).contains('Добавить').click();
     cy.get(constructorElementMain).should('not.exist');
+    expectIngredientInConstructor(meatId);
 
-    const number = '11111'; // поле number заказа
+    // Оформляем заказ
     cy.get(modal).should('not.exist');
     cy.get(burgerConstructor).contains('Оформить заказ').click();
-    cy.get(modal).should('exist');
-    cy.get(modal).should('contain', number);
+    cy.get(modal).should('exist').and('contain', orderNumber);
     cy.get(closeModalButton).click();
     cy.get(modal).should('not.exist');
   });
